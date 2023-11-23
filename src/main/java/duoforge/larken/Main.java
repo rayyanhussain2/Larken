1bb70db7771cf0d3a555267ab6e3a304aa144f50
rayyanhussain2 <rayyanhussain7@proton.me>
2023-11-23 21:17:21 +0530

    init

--- src/main/java/duoforge/larken/Main.java
+++ src/main/java/duoforge/larken/Main.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),  854, 480);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
